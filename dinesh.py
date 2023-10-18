7e78edba6584843ff680c3f06a3c30dac9fbfcf1
Dinesh <dineshsugumar289@gmail.com>
2023-10-18 16:30:32 +0000

    change5

--- dinesh.py
+++ dinesh.py
@@ -55,3 +55,29 @@ df4=df3['customer_name'].unique()
 
 df5=pd.DataFrame(df4)
 df5
+
+#now lets work on th second part
+df6=df2[df2["sales"]>250]
+df6
+
+#we can see that the num. of customers have gone down from 3898 to 2155 as we increased the high trans value to 250
+
+df6['order_id']
+
+df7=df6['customer_name'].unique()
+
+pd.DataFrame(df7)
+
+from datetime import datetime
+import numpy as np
+
+df6["month"]=pd.to_datetime(df6["order_purchase_date"])
+
+df6['Month1'] = df6["month"].dt.strftime('%m')
+df6['Year1'] = df6["month"].dt.strftime('%Y')
+df6
+
+df8=df6.groupby(['Month1','Year1']).agg({'sales':np.sum})
+df8.reset_index(inplace=True)
+
+df8
