f594cdb16f10201c05744ec6806d1fad0e6a5742
Dinesh <dineshsugumar289@gmail.com>
2023-10-18 14:35:44 +0000

    change3

--- dinesh.py
+++ dinesh.py
@@ -1,3 +1,8 @@
 
 print('hello')
 print('hii')
+
+
+import pandas as pd
+df=pd.read_csv('Downloads/156c0733-d225-4b76-a984-3ed5e19eb16d_83d04ac6-cb74-4a96-a06a-e0d5442aa126_customers.csv')
+df.head()
